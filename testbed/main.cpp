6942934c6b7e36efda03d344eca4649746d06d0c
vladeemerr <50291143+vladeemerr@users.noreply.github.com>
2025-10-01 09:16:47 +0300

    Provide head start code

--- testbed/main.cpp
+++ testbed/main.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-#include <optional>
 #include <cmath>
 
 #include <veekay/veekay.hpp>
@@ -13,7 +12,22 @@
 
 namespace {
 
-std::optional<VkShaderModule> loadShaderModule(const char* path) {
+struct ShaderConstants {
+};
+
+struct VulkanBuffer {
+	VkBuffer buffer;
+	VkDeviceMemory memory;
+};
+
+VkShaderModule vk_vertex_shader_module;
+VkShaderModule vk_fragment_shader_module;
+VkPipelineLayout vk_pipeline_layout;
+VkPipeline vk_pipeline;
+
+// NOTE: Declare buffers and other variables here
+
+VkShaderModule loadShaderModule(const char* path) {
 	std::ifstream file(path, std::ios::binary | std::ios::ate);
 	size_t size = file.tellg();
 	std::vector<uint32_t> buffer(size / sizeof(uint32_t));
@@ -30,22 +44,261 @@ std::optional<VkShaderModule> loadShaderModule(const char* path) {
 	VkShaderModule result;
 	if (vkCreateShaderModule(veekay::app.vk_device, &
 	                         info, nullptr, &result) != VK_SUCCESS) {
-		return std::nullopt;
+		return nullptr;
 	}
 
-	return std::make_optional<VkShaderModule>(result);
+	return result;
+}
+
+VulkanBuffer createBuffer(size_t size, void *data, VkBufferUsageFlags usage) {
+	VkDevice& device = veekay::app.vk_device;
+	VkPhysicalDevice& physical_device = veekay::app.vk_physical_device;
+	
+	VulkanBuffer result{};
+
+	{
+		VkBufferCreateInfo info{
+			.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+			.size = size,
+			.usage = usage,
+			.sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+		};
+
+		if (vkCreateBuffer(device, &info, nullptr, &result.buffer) != VK_SUCCESS) {
+			std::cerr << "Failed to create Vulkan buffer\n";
+			return {};
+		}
+	}
+
+	{
+		VkMemoryRequirements requirements;
+		vkGetBufferMemoryRequirements(device, result.buffer, &requirements);
+
+		VkPhysicalDeviceMemoryProperties properties;
+		vkGetPhysicalDeviceMemoryProperties(physical_device, &properties);
+
+		const VkMemoryPropertyFlags flags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+		                                    VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+
+		uint32_t index = UINT_MAX;
+		for (uint32_t i = 0; i < properties.memoryTypeCount; ++i) {
+			const VkMemoryType& type = properties.memoryTypes[i];
+
+			if ((requirements.memoryTypeBits & (1 << i)) &&
+			    (type.propertyFlags & flags) == flags) {
+				index = i;
+				break;
+			}
+		}
+
+		if (index == UINT_MAX) {
+			std::cerr << "Failed to find required memory type to allocate Vulkan buffer\n";
+			return {};
+		}
+
+		VkMemoryAllocateInfo info{
+			.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+			.allocationSize = requirements.size,
+			.memoryTypeIndex = index,
+		};
+
+		if (vkAllocateMemory(device, &info, nullptr, &result.memory) != VK_SUCCESS) {
+			std::cerr << "Failed to allocate Vulkan buffer memory\n";
+			return {};
+		}
+
+		if (vkBindBufferMemory(device, result.buffer, result.memory, 0) != VK_SUCCESS) {
+			std::cerr << "Failed to bind Vulkan  buffer memory\n";
+			return {};
+		}
+
+		void* device_data;
+		vkMapMemory(device, result.memory, 0, requirements.size, 0, &device_data);
+		memcpy(device_data, data, size);
+		vkUnmapMemory(device, result.memory);
+	}
+
+	return result;
+}
+
+void destroyBuffer(const VulkanBuffer& buffer) {
+	VkDevice& device = veekay::app.vk_device;
+
+	vkFreeMemory(device, buffer.memory, nullptr);
+	vkDestroyBuffer(device, buffer.buffer, nullptr);
 }
 
 void initialize() {
-	// TODO: Your Vulkan objects initialization here
+	VkDevice& device = veekay::app.vk_device;
+	VkPhysicalDevice& physical_device = veekay::app.vk_physical_device;
+
+	{ // NOTE: Build graphics pipeline
+		vk_vertex_shader_module = loadShaderModule("./shaders/shader.vert.spv");
+		if (!vk_vertex_shader_module) {
+			std::cerr << "Failed to load Vulkan vertex shader from file\n";
+			veekay::app.running = false;
+			return;
+		}
+
+		vk_fragment_shader_module = loadShaderModule("./shaders/shader.frag.spv");
+		if (!vk_fragment_shader_module) {
+			std::cerr << "Failed to load Vulkan fragment shader from file\n";
+			veekay::app.running = false;
+			return;
+		}
+
+		VkPipelineShaderStageCreateInfo stage_infos[2];
+
+		// NOTE: Vertex shader stage
+		stage_infos[0] = VkPipelineShaderStageCreateInfo{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+			// TODO
+		};
+
+		// NOTE: Fragment shader stage
+		stage_infos[1] = VkPipelineShaderStageCreateInfo{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+			// TODO
+		};
+
+		VkVertexInputBindingDescription buffer_binding{
+			// TODO
+		};
+
+		VkVertexInputAttributeDescription attributes[] = {
+			// TODO
+		};
+
+		VkPipelineVertexInputStateCreateInfo input_state_info{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+			// TODO
+		};
+
+		VkPipelineInputAssemblyStateCreateInfo assembly_state_info{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+			// TODO
+		};
+
+		VkPipelineRasterizationStateCreateInfo raster_info{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+			// TODO
+		};
+
+		VkPipelineMultisampleStateCreateInfo sample_info{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+			// TODO
+		};
+
+		VkPipelineColorBlendAttachmentState attachment_info{
+			.colorWriteMask = VK_COLOR_COMPONENT_R_BIT |
+			                  VK_COLOR_COMPONENT_G_BIT |
+			                  VK_COLOR_COMPONENT_B_BIT |
+			                  VK_COLOR_COMPONENT_A_BIT,
+		};
+
+		VkViewport viewport{
+			.x = 0.0f,
+			.y = 0.0f,
+			.width = static_cast<float>(veekay::app.window_width),
+			.height = static_cast<float>(veekay::app.window_height),
+			.minDepth = 0.0f,
+			.maxDepth = 1.0f,
+		};
+
+		VkRect2D scissor{
+			.offset = {0, 0},
+			.extent = {veekay::app.window_width, veekay::app.window_height},
+		};
+
+		VkPipelineViewportStateCreateInfo viewport_info{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+
+			.viewportCount = 1,
+			.pViewports = &viewport,
+
+			.scissorCount = 1,
+			.pScissors = &scissor,
+		};
+
+		VkPipelineDepthStencilStateCreateInfo depth_info{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
+			// TODO
+		};
+
+		VkPipelineColorBlendStateCreateInfo blend_info{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+
+			.logicOpEnable = false,
+			.logicOp = VK_LOGIC_OP_COPY,
+
+			.attachmentCount = 1,
+			.pAttachments = &attachment_info
+		};
+
+		VkPushConstantRange push_constants{
+			.stageFlags = VK_SHADER_STAGE_VERTEX_BIT |
+			              VK_SHADER_STAGE_FRAGMENT_BIT,
+			.size = sizeof(ShaderConstants),
+		};
+
+		VkPipelineLayoutCreateInfo layout_info{
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+			.pushConstantRangeCount = 1,
+			.pPushConstantRanges = &push_constants,
+		};
+
+		if (vkCreatePipelineLayout(device, &layout_info,
+		                           nullptr, &vk_pipeline_layout) != VK_SUCCESS) {
+			std::cerr << "Failed to create Vulkan pipeline layout\n";
+			veekay::app.running = false;
+			return;
+		}
+		
+		VkGraphicsPipelineCreateInfo info{
+			.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
+			.stageCount = 2,
+			.pStages = stage_infos,
+			.pVertexInputState = &input_state_info,
+			.pInputAssemblyState = &assembly_state_info,
+			.pViewportState = &viewport_info,
+			.pRasterizationState = &raster_info,
+			.pMultisampleState = &sample_info,
+			.pDepthStencilState = &depth_info,
+			.pColorBlendState = &blend_info,
+			.layout = vk_pipeline_layout,
+			.renderPass = veekay::app.vk_render_pass,
+		};
+
+		if (vkCreateGraphicsPipelines(device, nullptr,
+		                              1, &info, nullptr, &vk_pipeline) != VK_SUCCESS) {
+			std::cerr << "Failed to create Vulkan pipeline\n";
+			veekay::app.running = false;
+			return;
+		}
+	}
+
+	// TODO: You define model vertices and create buffers here
+	// TODO: Index buffer has to be created here too
+	// NOTE: Look for createBuffer function
 }
 
 void shutdown() {
-	// TODO: Your clean up code here
+	VkDevice& device = veekay::app.vk_device;
+
+	// NOTE: Destroy resources here, do not cause leaks in your program!
+
+	vkDestroyPipeline(device, vk_pipeline, nullptr);
+	vkDestroyPipelineLayout(device, vk_pipeline_layout, nullptr);
+	vkDestroyShaderModule(device, vk_fragment_shader_module, nullptr);
+	vkDestroyShaderModule(device, vk_vertex_shader_module, nullptr);
 }
 
 void update(double time) {
+	ImGui::Begin("Controls:");
 	// TODO: Your GUI and other stuff here
+	ImGui::End();
+
+	// NOTE: Animation code and other runtime variable updates go here
 }
 
 void render(VkCommandBuffer cmd, VkFramebuffer framebuffer) {
@@ -60,8 +313,35 @@ void render(VkCommandBuffer cmd, VkFramebuffer framebuffer) {
 		vkBeginCommandBuffer(cmd, &info);
 	}
 
-	// TODO: Vulkan rendering code here
+	{ // NOTE: Use current swapchain framebuffer and clear it
+		VkClearValue clear_color{.color = {{0.1f, 0.1f, 0.1f, 1.0f}}};
+		VkClearValue clear_depth{.depthStencil = {1.0f, 0}};
 
+		VkClearValue clear_values[] = {clear_color, clear_depth};
+
+		VkRenderPassBeginInfo info{
+			.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+			.renderPass = veekay::app.vk_render_pass,
+			.framebuffer = framebuffer,
+			.renderArea = {
+				.extent = {
+					veekay::app.window_width,
+					veekay::app.window_height
+				},
+			},
+			.clearValueCount = 2,
+			.pClearValues = clear_values,
+		};
+
+		vkCmdBeginRenderPass(cmd, &info, VK_SUBPASS_CONTENTS_INLINE);
+	}
+
+	{
+		// TODO: Vulkan rendering code here
+		// NOTE: ShaderConstant updates, vkCmdXXX expected to be here
+	}
+
+	vkCmdEndRenderPass(cmd);
 	vkEndCommandBuffer(cmd);
 }
 
